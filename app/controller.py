7ac008cf3025238d7086b353a62053bea1acd5e9
glorne <geo.lorne@gmail.com>
2016-02-20 14:51:38 -0800

    hcapes dank code

---
+++ app/controller.py
@@ -0,0 +1,9 @@
+def nearestFood(data):
+    #returns cloest food
+    return -1
+def moveTowards(data):
+    #move towards a cordinate
+    return -1
+def isSafe(data):
+    #determines in move is safe
+    return -1
--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/suh.png' % (
+    head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
@@ -27,33 +27,43 @@ def start():
     # TODO: Do things with data
 
     return {
-        'taunt': 'aha asuh dude'
+        'taunt': 'battlesnake-python!'
     }
 
-x=0
+
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    #  TODO: Do things with data
-    # if(x == 1):
-    #     x = 0
-    #     return {
-    #         'move': 'north',
-    #         'taunt': 'battlesnake-python!'
-    #     }
-    # if x==0:
-    #     x=1
-    #     return {
-    #         'move': 'west',
-    #         'taunt': 'battlesnake-python!'
-    #     }
-    # if (turtle):
-    print "data = bottle.request.json"
-    print data
-    return {
-     'move': 'east',
-     'taunt': data.game
-          }
+    bool foodMode = False
+    # TODO: Do things with data
+
+    if data.snakes.id[299642ef-7497-4b0b-9d46-5e8df951df5c].health < 30:
+        foodMode = True
+
+    if foodMode:
+        directionsToFood = shortestCalcFoodDistance(data.food, data.snakes.id[299642ef-7497-4b0b-9d46-5e8df951df5c].coords[0]);
+
+        if (directionsToFood[0] > 0):
+            return {
+                'move': 'west',
+                'taunt': 'battlesnake-python!'
+            }
+        else if (directionsToFood[0] < 0):
+            return {
+                'move': 'east',
+                'taunt': 'battlesnake-python!'
+            }
+        else if (directionsToFood[1] > 0):
+            return {
+                'move': 'north',
+                'taunt': 'battlesnake-python!'
+            }
+        else if (directionsToFood[1] < 0):
+            return {
+                'move': 'south',
+                'taunt': 'battlesnake-python!'
+            }
+
 
 @bottle.post('/end')
 def end():
@@ -70,3 +80,22 @@ def end():
 application = bottle.default_app()
 if __name__ == '__main__':
     bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+
+
+def calcDistance(coord, snakehead){
+    int x = snakehead[0] - coord[0]
+    int y = snakehead[1] - coord[1]
+    return([x,y])
+}
+
+def shortestCalcFoodDistance(coord, snakehead){
+    int distance = 100
+    desiredFood = [0,0]
+    for food in coord:
+        int x = snakehead[0] - coord[0]
+        int y = snakehead[1] - coord[1]
+        if abs(x+y) < distance:
+            int distnace = abs(x+y)
+            desiredFood = [x,y]
+    return desiredFood
+}
